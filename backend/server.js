d99377f607a1a46a82bc67cea5c8dd8b3dffd5ab
bharathjinka09 <bharathjinka09@gmail.com>
2020-10-06 14:31:36 +0530

    Creating Models

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import products from "./data/products.js";
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+import colors from "colors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -26,5 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
